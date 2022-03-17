1c7a79eae15931f204b4ed27bcd4b65b740e9237
Dasom Ahn <lizzy0521@naver.com>
2022-03-17 23:43:50 +0900

    [Bronze V] Title: 강아지, Time: 140 ms, Memory: 15696 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/강아지.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String []args){ 
+        char symbol='\u0060';
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\""+ symbol + "    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
