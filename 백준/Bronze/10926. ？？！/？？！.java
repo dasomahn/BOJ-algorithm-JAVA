af0a755a70c87dce4da7d89803b45f313571f8d1
donttell <58074467+donttell@users.noreply.github.com>
2022-03-31 16:51:39 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        String a = scan.nextLine();
+        scan.close();
+        
+        System.out.println(a+"??!");
+    }
+}
