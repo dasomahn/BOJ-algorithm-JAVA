d4e743a16efb7540576228b94e2837b895750dea
donttell <58074467+donttell@users.noreply.github.com>
2022-03-31 16:54:09 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        System.out.println(a-543);
+
+        scan.close();
+    }
+}
